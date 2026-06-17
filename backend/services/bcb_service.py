284992f5a472ab5fe7598cd475c2d5924770397b
mathe <matheusmen58@gmail.com>
2026-06-17 11:34:54 -0300

    fix(services): ajusta busca do dólar para rota por período do BACEN

--- backend/services/bcb_service.py
+++ backend/services/bcb_service.py
@@ -1,20 +1,27 @@
 import requests
+from datetime import datetime, timedelta
 
-SGS_BASE_URL = (
-    "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados/ultimos/{quantidade}?formato=json"
-)
+# Endpoint para os últimos registros
+SGS_ULTIMOS_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados/ultimos/{quantidade}?formato=json"
+
+# Endpoint por período para a tratação de erro do dolar.
+SGS_PERIODO_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados?formato=json&dataInicial={inicio}&dataFinal={fim}"
 
 def buscar_indicador(codigo, quantidade):
     try:
-        response = requests.get(
-            SGS_BASE_URL.format(
-                codigo=codigo,
-                quantidade=quantidade
-            )
-        )
+        if codigo == 1:
+            hoje = datetime.now()
+            trinta_dias_atras = hoje - timedelta(days=30)
+            
+            data_inicio = trinta_dias_atras.strftime("%d/%m/%Y")
+            data_fim = hoje.strftime("%d/%m/%Y")
+            
+            url = SGS_PERIODO_URL.format(codigo=codigo, inicio=data_inicio, fim=data_fim)
+        else:
+            url = SGS_ULTIMOS_URL.format(codigo=codigo, quantidade=quantidade)
 
+        response = requests.get(url)
         response.raise_for_status()
-
         return response.json()
 
     except requests.exceptions.RequestException as e:
